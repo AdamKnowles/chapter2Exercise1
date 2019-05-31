7e5d3e32ca614dea226405481640c541e3543274
Adam Knowles <acknowles524@gmail.com>
2019-05-31 14:00:18 -0500

    initial commit

---
+++ chapter2Practice/chapter2.js
@@ -0,0 +1,15 @@
+ const selection = document.querySelector(".article__header");
+ selection.textContent = "Welcome to Adam's blog";
+
+ const selectionAll = document.querySelectorAll(".article__header");
+ selectionAll.classList = "article__header important";
+
+ const removeClass = document.querySelector(".dashed");
+ removeClass.classList.remove("dashed");
+
+ let addClass = document.querySelector(".article__footer");
+ addClass.classList.add("goldenrod");
+
+
+
+
